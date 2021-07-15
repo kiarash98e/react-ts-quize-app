694f461d3549eb3cf2dd753dc32f37b3705e7b4f
kiarashsafaei98 <kiarashsafaei0@gmail.com>
2021-07-15 13:17:43 +0430

    api call by fetch

---
+++ src/Api.ts
@@ -0,0 +1,28 @@
+
+import {shufleArray} from "./utils"
+
+export type Questions = {
+    category:string
+    correct_answer:string
+    difficulty:string
+    incorrect_answer:string[]
+    question:string
+    type:string
+}
+
+export type QuestionState = Questions & {answers:string[]}
+
+export enum Difficulty {
+     EASY = 'easy',
+     MEDIUM = 'medium',
+     HARD = 'hard'
+ }
+
+ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionState[]> => {
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
+    const data = await (await fetch(endpoint)).json()
+    return data.results.map((question: Questions) => ({
+      ...question,
+      answers: shufleArray([...question.incorrect_answer, question.correct_answer])
+    }))
+  }
